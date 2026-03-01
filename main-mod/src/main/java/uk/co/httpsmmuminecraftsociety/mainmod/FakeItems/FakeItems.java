52480652c4d07e7c9d430472ef25409daee45831
FreddyMSchubert <github@frederickschubert.de>
2026-03-01 16:26:24 +0000

    add more cosmetic hats, a bunch of villager hats

--- main-mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/FakeItems/FakeItems.java
+++ main-mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/FakeItems/FakeItems.java
@@ -2,6 +2,7 @@ package uk.co.httpsmmuminecraftsociety.mainmod.FakeItems;
 
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
@@ -15,19 +16,26 @@ public final class FakeItems {
     private FakeItems() {}
 
     public static final List<FakeItemDef> ALL = List.of(
-            createFakeItemDef("coin-1",      "1 Dabloon",    "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
-            createFakeItemDef("coin-5",      "5 Dabloons",   "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
-            createFakeItemDef("coin-10",     "10 Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
-            createFakeItemDef("coin-50",     "50 Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
-            createFakeItemDef("coin-100",    "100 Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
-            createFakeItemDef("coin-500",    "500 Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
-            createFakeItemDef("coin-1000",   "1k Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
-            createFakeItemDef("coin-5000",   "5k Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
-            createFakeItemDef("coin-10000",  "10k Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
-            createFakeItemDef("coin-50000",  "50k Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
-            createFakeItemDef("coin-100000", "100k Dabloons","Official MMU Minecraft Society Mint Issue", Rarity.EPIC, 67),
-            createFakeItemDef("coin-500000", "500k Dabloons","Official MMU Minecraft Society Mint Issue", Rarity.EPIC, 67),
-            createFakeItemDef("coin-1000000","1m Dabloons",  "With all the money in the world, you still can't buy yourself a soul.", Rarity.EPIC, 67)
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-1",      "1 Dabloon",    "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-5",      "5 Dabloons",   "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-10",     "10 Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-50",     "50 Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.COMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-100",    "100 Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-500",    "500 Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-1000",   "1k Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.UNCOMMON, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-5000",   "5k Dabloons",  "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-10000",  "10k Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-50000",  "50k Dabloons", "Official MMU Minecraft Society Mint Issue", Rarity.RARE, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-100000", "100k Dabloons","Official MMU Minecraft Society Mint Issue", Rarity.EPIC, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-500000", "500k Dabloons","Official MMU Minecraft Society Mint Issue", Rarity.EPIC, 67),
+            createFakeItemDef(Items.COMMAND_BLOCK, "coin-1000000","1m Dabloons",  "With all the money in the world, you still can't buy yourself a soul.", Rarity.EPIC, 67),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-armorer","Armorer Goggles",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-butcher","Butcher Headband",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-farmer","Farmer Straw hat",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-fisherman","Fisherman Hat",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-fletcher","Fletcher Hat",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-librarian","Librarian Hat",  "", Rarity.COMMON, 1),
+            createFakeItemDef(Items.CARVED_PUMPKIN, "cosmetic-hat-villager-shepherd","Shepherd Hat",  "", Rarity.COMMON, 1)
     );
     private static final Map<String, FakeItemDef> BY_ID =
             ALL.stream().collect(Collectors.toUnmodifiableMap(FakeItemDef::id, d -> d));
@@ -43,7 +51,7 @@ public final class FakeItems {
     }
     public static ItemStack createStack(String id, int amount) {
         FakeItemDef d = def(id);
-        ItemStack stack = new ItemStack(Items.COMMAND_BLOCK, amount);
+        ItemStack stack = new ItemStack(d.baseItem, amount);
 
         applyComponents(stack, d);
 
@@ -52,6 +60,7 @@ public final class FakeItems {
 
 
     public record FakeItemDef(
+            Item baseItem,
             String id,
             Component title,
             List<Component> lore,
@@ -59,15 +68,16 @@ public final class FakeItems {
             int maxStackSize
     ) {}
 
-    private static FakeItemDef createFakeItemDef(String id,  String title, String loreLine, Rarity rarity, int maxStackSize) {
-        return new FakeItemDef(id, Component.literal(title), List.of(Component.literal(loreLine)), rarity, maxStackSize);
+    private static FakeItemDef createFakeItemDef(Item baseItem, String id, String title, String loreLine, Rarity rarity, int maxStackSize) {
+        return new FakeItemDef(baseItem, id, Component.literal(title), List.of(Component.literal(loreLine)), rarity, maxStackSize);
     }
 
 
     private static void applyComponents(ItemStack stack, FakeItemDef d) {
         stack.set(DataComponents.CUSTOM_MODEL_DATA, new CustomModelData(List.of(), List.of(), List.of(d.id()), List.of()));
         stack.set(DataComponents.CUSTOM_NAME, d.title());
-        stack.set(DataComponents.LORE, new ItemLore(d.lore()));
+        if (d.lore() != null && !d.lore().isEmpty())
+            stack.set(DataComponents.LORE, new ItemLore(d.lore()));
         stack.set(DataComponents.RARITY, d.rarity());
         stack.set(DataComponents.MAX_STACK_SIZE, d.maxStackSize());
     }

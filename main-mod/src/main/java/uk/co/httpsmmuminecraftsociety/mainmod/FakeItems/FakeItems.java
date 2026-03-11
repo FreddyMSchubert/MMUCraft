361fce8152c63700c0857b29281b06ec7a6a6e71
FreddyMSchubert <github@frederickschubert.de>
2026-03-11 21:23:21 +0000

    Animated propeller hat cosmetic

--- main-mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/FakeItems/FakeItems.java
+++ main-mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/FakeItems/FakeItems.java
@@ -47,6 +47,7 @@ public final class FakeItems {
             new CosmeticFakeItem       ("cosmetic-hat-skull",             "Skull Hat",          Rarity.COMMON),
             new CosmeticFakeItem       ("cosmetic-hat-bunny-ears",        "Bunny Ears",         Rarity.COMMON),
             new CosmeticFakeItem       ("cosmetic-hat-cheese",            "Cheese Hat",         Rarity.COMMON),
+            new CosmeticFakeItem       ("cosmetic-hat-propeller-hat",     "Propeller Hat",      Rarity.COMMON),
             new DyeableCosmeticFakeItem("cosmetic-hat-spartan-helmet",    "Spartan Helmet",     Rarity.COMMON, Utils.rgbToMinecraftColor(0, 0, 255)),
             new DyeableCosmeticFakeItem("cosmetic-hat-amogus-hat",        "Amogus Hat",         Rarity.COMMON, Utils.rgbToMinecraftColor(255, 0, 0)),
             new DyeableCosmeticFakeItem("cosmetic-hat-devil-horns",       "Devil Horns",        Rarity.COMMON, Utils.rgbToMinecraftColor(255, 0, 0)),

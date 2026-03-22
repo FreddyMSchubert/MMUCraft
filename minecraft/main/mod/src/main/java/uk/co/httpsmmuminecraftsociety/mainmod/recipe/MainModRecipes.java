ca91ddbd6a8c28ae7d73e5d7b0ab8cc29ca3e9d4
FreddyMSchubert <github@frederickschubert.de>
2026-03-22 08:54:04 +0000

    reworked coin recipes to use new system

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/recipe/MainModRecipes.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/recipe/MainModRecipes.java
@@ -11,7 +11,6 @@ import uk.co.httpsmmuminecraftsociety.mainmod.recipe.dataDriven.FakeShapelessCra
 public final class MainModRecipes {
     private MainModRecipes() {}
 
-    public static final RecipeSerializer<CoinConvertRecipe> COIN_CONVERT_SERIALIZER = new CoinConvertRecipe.Serializer();
     public static final RecipeSerializer<CombineCharmorRecipe> COMBINE_CHARMOR_SERIALIZER = new CombineCharmorRecipe.Serializer();
     public static final RecipeSerializer<SeperateCharmorRecipe> SEPERATE_CHARMOR_SERIALIZER = new SeperateCharmorRecipe.Serializer();
     public static final RecipeSerializer<CombineCosmeticRecipe> COMBINE_COSMETIC_SERIALIZER = new CombineCosmeticRecipe.Serializer();
@@ -21,7 +20,6 @@ public final class MainModRecipes {
     public static final RecipeSerializer<FakeShapelessCraftingRecipe> FAKE_CRAFTING_SHAPELESS_SERIALIZER = new FakeShapelessCraftingRecipe.Serializer();
 
     public static void register() {
-        Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, Identifier.fromNamespaceAndPath(MainMod.MOD_ID, "coin_convert"), COIN_CONVERT_SERIALIZER);
         Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, Identifier.fromNamespaceAndPath(MainMod.MOD_ID, "combine_charmor"), COMBINE_CHARMOR_SERIALIZER);
         Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, Identifier.fromNamespaceAndPath(MainMod.MOD_ID, "seperate_charmor"), SEPERATE_CHARMOR_SERIALIZER);
         Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, Identifier.fromNamespaceAndPath(MainMod.MOD_ID, "combine_cosmetic"), COMBINE_COSMETIC_SERIALIZER);

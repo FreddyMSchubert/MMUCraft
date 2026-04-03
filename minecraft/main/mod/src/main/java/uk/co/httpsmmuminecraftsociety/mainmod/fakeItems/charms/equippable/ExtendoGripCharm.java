8c4ea3e1112b1ab09ce6ed1a7011ceba06bb438d
FreddyMSchubert <github@frederickschubert.de>
2026-04-03 09:54:12 +0200

    candle of the deep, extendo grip, goop hand balancing tweaks

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/fakeItems/charms/equippable/ExtendoGripCharm.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/fakeItems/charms/equippable/ExtendoGripCharm.java
@@ -10,15 +10,23 @@ import uk.co.httpsmmuminecraftsociety.mainmod.utils.Utils;
 
 public class ExtendoGripCharm implements Charm, BaseItemChangeCallbackCharm
 {
+    private static final String EXTENDO_GRIP_BLOCK_INTERACTION_RANGE_ATTRIBUTE_MODIFIER_ID = "extendo_grip_block_interaction_range";
+
     @Override
     public void enableEffectForItem(ItemStack stack, int charmLevel)
     {
-        Utils.applyItemAttrModifier(stack, "extendo_grip_block_interaction_range", Attributes.BLOCK_INTERACTION_RANGE, 1.999, AttributeModifier.Operation.ADD_VALUE, EquipmentSlotGroup.CHEST);
+        Utils.applyItemAttrModifier(stack, EXTENDO_GRIP_BLOCK_INTERACTION_RANGE_ATTRIBUTE_MODIFIER_ID, Attributes.BLOCK_INTERACTION_RANGE, getBlockInteractionRangeAdditionPerLevel(charmLevel), AttributeModifier.Operation.ADD_VALUE, EquipmentSlotGroup.CHEST);
     }
 
     @Override
     public void disableEffectForItem(ItemStack stack, int charmLevel)
     {
-        Utils.removeItemAttrModifier(stack, "extendo_grip_block_interaction_range", Attributes.BLOCK_INTERACTION_RANGE);
+        Utils.removeItemAttrModifier(stack, EXTENDO_GRIP_BLOCK_INTERACTION_RANGE_ATTRIBUTE_MODIFIER_ID, Attributes.BLOCK_INTERACTION_RANGE);
+    }
+
+    private static float getBlockInteractionRangeAdditionPerLevel(int level) {
+        if (level < 9)
+            return level;
+        return 15;
     }
 }

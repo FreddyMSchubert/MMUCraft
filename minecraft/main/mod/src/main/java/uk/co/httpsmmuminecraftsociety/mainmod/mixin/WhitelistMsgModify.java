a1660cc1ea6dcdda8fc9778675a50fce4beff4e8
FreddyMSchubert <github@frederickschubert.de>
2026-04-09 12:00:00 +0100

    whitelist message formatting improvements

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/mixin/WhitelistMsgModify.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/mixin/WhitelistMsgModify.java
@@ -1,6 +1,9 @@
 package uk.co.httpsmmuminecraftsociety.mainmod.mixin;
 
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.players.NameAndId;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,20 +13,37 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.net.SocketAddress;
+import java.net.URI;
 
 @Mixin(PlayerList.class)
 public class WhitelistMsgModify
 {
     @Unique
     private static Component customWhitelistMessage() {
-        return Component.literal(
-                "Hi and Welcome!\n" +
-                        "To make this server super-secure, we use a whitelist to block hackers.\n" +
-                        "You are not currently on the whitelist.\n" +
-                        "This can be changed in under a minute by verifying your MMU email here:\n" +
-                        "https://mmuminecraftsociety.co.uk/\n" +
-                        "See you then ;)"
-        );
+        return Component.empty()
+                .append(Component.literal("⬆️ Don't worry, that is normal.\n\n"))
+                .append(Component.literal("Hi and welcome to the MMU Minecraft Society! :)\n")
+                        .withStyle(style -> style
+                                .withColor(0xFFD166)
+                                .withBold(true)))
+                .append(Component.literal("This server uses a whitelist to keep hackers, demons and the "))
+                .append(Component.literal("chupacabra")
+                        .withStyle(style -> style.withObfuscated(true)))
+                .append(Component.literal(" at bay.\n"))
+                .append(Component.literal("You are not currently on the whitelist.\n\n")
+                        .withStyle(style -> style
+                                .withColor(0xFF6B6B)
+                                .withBold(true)))
+                .append(Component.literal("To change this in under 60 seconds, please verify your MMU email here:\n")
+                .append(Component.literal("mmuminecraftsociety.co.uk\n\n")
+                        .withStyle(style -> style
+                                .withColor(0x0073E6)
+                                .withUnderlined(true)
+                                .withHoverEvent(new HoverEvent.ShowText(
+                                        Component.literal("ermmm my bad i don't think it's clickable please type the url")
+                                ))))
+                .append(Component.literal("Once you're on the whitelist, you'll be able to join.\n"))
+                .append(Component.literal("See you then! :)").withStyle(style -> style.withBold(true).withColor(0xFFD166))));
     }
 
     @Inject(method = "canPlayerLogin", at = @At("RETURN"), cancellable = true)

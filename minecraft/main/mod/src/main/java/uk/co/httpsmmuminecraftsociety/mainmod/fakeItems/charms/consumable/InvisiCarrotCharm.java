1d326064bc523907432c3edfbe9028e3c22415a0
FreddyMSchubert <github@frederickschubert.de>
2026-04-10 02:38:24 +0200

    add invisi carrot

---
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/fakeItems/charms/consumable/InvisiCarrotCharm.java
@@ -0,0 +1,34 @@
+package uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.consumable;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import org.jspecify.annotations.Nullable;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.def.Charm;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.def.UseEntityCharm;
+
+public class InvisiCarrotCharm implements Charm, UseEntityCharm
+{
+    @Override
+    public InteractionResult onUseEntity(ItemStack stack, Player player, Level level, InteractionHand interactionHand, Entity entity, @Nullable EntityHitResult entityHitResult, int charmLevel)
+    {
+        if (!(entity instanceof ItemFrame frame)) {
+            return InteractionResult.PASS;
+        }
+
+        if (frame.isInvisible()) {
+            return InteractionResult.PASS;
+        }
+
+        frame.setInvisible(true);
+
+        stack.shrink(1);
+
+        return InteractionResult.SUCCESS_SERVER;
+    }
+}
---
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/fakeItems/charms/def/UseEntityCharm.java
@@ -0,0 +1,15 @@
+package uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.def;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import org.jspecify.annotations.Nullable;
+
+public interface UseEntityCharm extends Charm
+{
+    InteractionResult onUseEntity(ItemStack stack, Player player, Level level, InteractionHand interactionHand, Entity entity, @Nullable EntityHitResult entityHitResult, int charmLevel);
+}

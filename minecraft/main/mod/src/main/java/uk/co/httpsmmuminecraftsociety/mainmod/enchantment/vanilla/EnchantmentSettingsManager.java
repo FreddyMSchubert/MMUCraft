071ca9ea16df53a38f5b1bc2cb7d00c1641db663
FreddyMSchubert <github@frederickschubert.de>
2026-05-24 22:24:29 +0100

    Enchanted book duping with right materials, enchanting loottable validation, better knowledge frontend display

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/enchantment/vanilla/EnchantmentSettingsManager.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/enchantment/vanilla/EnchantmentSettingsManager.java
@@ -1,8 +1,10 @@
 package uk.co.httpsmmuminecraftsociety.mainmod.enchantment.vanilla;
 
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -21,82 +23,88 @@ Epic - 12.5 %
 
 public class EnchantmentSettingsManager
 {
-    private static final List<String> trialChamberRewards = List.of("trial_chambers/reward", "trial_chambers/reward_common", "trial_chambers/reward_ominous", "trial_chambers/reward_ominous_common", "trial_chambers/reward_ominous_rare", "trial_chambers/reward_rare");
-    private static final List<String> bastionRewards = List.of("bastion_bridge", "bastion_hoglin_stable", "bastion_other", "bastion_treasure");
+    private static final List<String> trialChamberRewards = List.of("chests/trial_chambers/reward", "chests/trial_chambers/reward_common", "chests/trial_chambers/reward_ominous", "chests/trial_chambers/reward_ominous_common", "chests/trial_chambers/reward_ominous_rare", "chests/trial_chambers/reward_rare");
+    private static final List<String> bastionRewards = List.of("chests/bastion_bridge", "chests/bastion_hoglin_stable", "chests/bastion_other", "chests/bastion_treasure");
+    public static final List<ResourceKey<Enchantment>> curses = List.of(
+        Enchantments.VANISHING_CURSE,
+        Enchantments.BINDING_CURSE,
+        ModEnchantments.FRAGILITY_CURSE
+    );
 
     public static final List<EnchantmentSettings> enchantmentSettings = List.of(
         // general enchantments
-        new EnchantmentSettings(Enchantments.UNBREAKING).inLoottable("end_city_treasure").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.MENDING).inLoottable("ancient_city", "ancient_city_ice_box").rarity(Rarity.RARE),
-        new EnchantmentSettings(ModEnchantments.SOULBOUND), // obtainable via crafting
+        new EnchantmentSettings(Enchantments.UNBREAKING).inLoottable("chests/end_city_treasure").rarity(Rarity.RARE).dupedWithVanillaItem(Items.TOTEM_OF_UNDYING),
+        new EnchantmentSettings(Enchantments.MENDING).inLoottable("chests/ancient_city", "chests/ancient_city_ice_box").rarity(Rarity.RARE).dupedWithVanillaItem(Items.EXPERIENCE_BOTTLE),
+        new EnchantmentSettings(ModEnchantments.SOULBOUND).dupedWithFakeItem("soul"), // obtainable via crafting
 
         // aquatic enchantments
-        new EnchantmentSettings(Enchantments.AQUA_AFFINITY).inLoottable("entities/guardian").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.DEPTH_STRIDER).inLoottable("underwater_ruin_big", "underwater_ruin_small").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.RESPIRATION).inLoottable("shipwreck_treasure", "shipwreck_supply", "shipwreck_map").rarity(Rarity.RARE),
+        new EnchantmentSettings(Enchantments.AQUA_AFFINITY).inLoottable("entities/guardian").rarity(Rarity.RARE).dupedWithVanillaItem(Items.SEA_LANTERN),
+        new EnchantmentSettings(Enchantments.DEPTH_STRIDER).inLoottable("chests/underwater_ruin_big", "chests/underwater_ruin_small").rarity(Rarity.RARE).dupedWithVanillaItem(Items.PRISMARINE_SHARD, 3),
+        new EnchantmentSettings(Enchantments.RESPIRATION).inLoottable("chests/shipwreck_treasure", "chests/shipwreck_supply", "chests/shipwreck_map").rarity(Rarity.RARE).dupedWithVanillaItem(Items.TURTLE_HELMET),
 
         // general armour enchantments
-        new EnchantmentSettings(Enchantments.FEATHER_FALLING).inLoottable("desert_pyramid").rarity(Rarity.UNCOMMON),
-        new EnchantmentSettings(Enchantments.THORNS).inLoottable("simple_dungeon"),
-        new EnchantmentSettings(ModEnchantments.CHARM_BOOST), // TODO: obtainable via archaeology loottables
-        new EnchantmentSettings(Enchantments.PROTECTION).inLoottable("end_city_treasure").rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.BLAST_PROTECTION).maxEnchTableLvl(1),
-        new EnchantmentSettings(Enchantments.FIRE_PROTECTION).maxEnchTableLvl(1),
-        new EnchantmentSettings(Enchantments.PROJECTILE_PROTECTION).maxEnchTableLvl(1),
+        new EnchantmentSettings(Enchantments.FEATHER_FALLING).inLoottable("chests/desert_pyramid").rarity(Rarity.UNCOMMON), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.THORNS).inLoottable("chests/simple_dungeon").dupedWithVanillaItem(Items.CACTUS_FLOWER, 2),
+        new EnchantmentSettings(ModEnchantments.CHARM_BOOST).dupedWithVanillaItem(Items.RAW_GOLD_BLOCK), // TODO: obtainable via archaeology loottables
+        new EnchantmentSettings(Enchantments.PROTECTION).inLoottable("chests/end_city_treasure").rarity(Rarity.EPIC).dupedWithVanillaItem(Items.ARMADILLO_SCUTE, 5),
+        new EnchantmentSettings(Enchantments.BLAST_PROTECTION).maxEnchTableLvl(1).dupedWithVanillaItem(Items.ARMADILLO_SCUTE, 3),
+        new EnchantmentSettings(Enchantments.FIRE_PROTECTION).maxEnchTableLvl(1).dupedWithVanillaItem(Items.ARMADILLO_SCUTE, 3),
+        new EnchantmentSettings(Enchantments.PROJECTILE_PROTECTION).maxEnchTableLvl(1).dupedWithVanillaItem(Items.ARMADILLO_SCUTE, 3),
 
         // mace enchantments
-        new EnchantmentSettings(Enchantments.BREACH).inLoottable(trialChamberRewards).rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.DENSITY).inLoottable(trialChamberRewards).rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.WIND_BURST).inLoottable(trialChamberRewards).rarity(Rarity.EPIC),
+        new EnchantmentSettings(Enchantments.BREACH).inLoottable(trialChamberRewards).rarity(Rarity.EPIC).dupedWithVanillaItem(Items.POINTED_DRIPSTONE, 10),
+        new EnchantmentSettings(Enchantments.DENSITY).inLoottable(trialChamberRewards).rarity(Rarity.EPIC).dupedWithVanillaItem(Items.ANVIL),
+        new EnchantmentSettings(Enchantments.WIND_BURST).inLoottable(trialChamberRewards).rarity(Rarity.EPIC).dupedWithVanillaItem(Items.BREEZE_ROD, 3),
 
         // trident enchantments
-        new EnchantmentSettings(Enchantments.CHANNELING).inLoottable("zombie_nautilus"),
-        new EnchantmentSettings(Enchantments.IMPALING).inLoottable("zombie_nautilus"),
-        new EnchantmentSettings(Enchantments.LOYALTY).inLoottable("entities/drowned").rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.RIPTIDE).inLoottable("entities/drowned").rarity(Rarity.EPIC),
+        // TODO: maybe swap these with shipwreck stuff? give each to one shipwreck chest and put the two incompatible ones into the same chest... but then where to put respiration lol? i just dislike entities dropping books
+        new EnchantmentSettings(Enchantments.CHANNELING).inLoottable("entities/zombie_nautilus").dupedWithVanillaItem(Items.WAXED_OXIDIZED_LIGHTNING_ROD),
+        new EnchantmentSettings(Enchantments.IMPALING).inLoottable("entities/zombie_nautilus").dupedWithVanillaItem(Items.NAUTILUS_SHELL),
+        new EnchantmentSettings(Enchantments.LOYALTY).inLoottable("entities/drowned").rarity(Rarity.EPIC), // TODO: duped by dog collar from killing a tamed dog
+        new EnchantmentSettings(Enchantments.RIPTIDE).inLoottable("entities/drowned").rarity(Rarity.EPIC).dupedWithVanillaItem(Items.PHANTOM_MEMBRANE, 3),
 
         // tool enchantments
-        new EnchantmentSettings(Enchantments.EFFICIENCY).inLoottable("stronghold_corridor", "stronghold_crossing", "stronghold_library").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.SILK_TOUCH).inLoottable("abandoned_mineshaft").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.FORTUNE), // TODO: obtained by crafting
+        new EnchantmentSettings(Enchantments.EFFICIENCY).inLoottable("chests/stronghold_corridor", "chests/stronghold_crossing", "chests/stronghold_library").rarity(Rarity.RARE).dupedWithVanillaItem(Items.TNT),
+        new EnchantmentSettings(Enchantments.SILK_TOUCH).inLoottable("chests/abandoned_mineshaft").rarity(Rarity.RARE).dupedWithVanillaItem(Items.BRAIN_CORAL_BLOCK),
+        new EnchantmentSettings(Enchantments.FORTUNE).dupedWithVanillaItem(Items.NETHERITE_SCRAP), // TODO: obtained by crafting
 
         // sword & spear enchantments
-        new EnchantmentSettings(Enchantments.LOOTING).inLoottable("nether_bridge").rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.FIRE_ASPECT).inLoottable("ruined_portal").rarity(Rarity.UNCOMMON),
-        new EnchantmentSettings(Enchantments.KNOCKBACK).inLoottable("woodland_mansion").rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.SHARPNESS).maxEnchTableLvl(1).rarity(Rarity.EPIC),
-        new EnchantmentSettings(Enchantments.SWEEPING_EDGE).inLoottable("woodland_mansion"),
-        new EnchantmentSettings(Enchantments.SMITE).maxEnchTableLvl(1),
-        new EnchantmentSettings(Enchantments.BANE_OF_ARTHROPODS).maxEnchTableLvl(1),
-        new EnchantmentSettings(Enchantments.LUNGE).inLoottable("desert_temple").rarity(Rarity.RARE),
+        new EnchantmentSettings(Enchantments.LOOTING).inLoottable("chests/nether_bridge").rarity(Rarity.RARE), // TODO: deupeability
+        new EnchantmentSettings(Enchantments.FIRE_ASPECT).inLoottable("chests/ruined_portal").rarity(Rarity.UNCOMMON).dupedWithVanillaItem(Items.FIRE_CHARGE),
+        new EnchantmentSettings(Enchantments.KNOCKBACK).inLoottable("chests/woodland_mansion").rarity(Rarity.EPIC).dupedWithVanillaItem(Items.SLIME_BLOCK),
+        new EnchantmentSettings(Enchantments.SHARPNESS).maxEnchTableLvl(1).rarity(Rarity.EPIC), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.SWEEPING_EDGE).inLoottable("chests/woodland_mansion"), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.SMITE).maxEnchTableLvl(1).dupedWithVanillaItem(Items.NETHER_STAR),
+        new EnchantmentSettings(Enchantments.BANE_OF_ARTHROPODS).maxEnchTableLvl(1).dupedWithVanillaItem(Items.FERMENTED_SPIDER_EYE, 5),
+        new EnchantmentSettings(Enchantments.LUNGE).inLoottable("chests/desert_pyramid").rarity(Rarity.RARE), // TODO: dupeability
 
         // boot enchantments
-        new EnchantmentSettings(Enchantments.FROST_WALKER).inLoottable("igloo_chest"),
-        new EnchantmentSettings(Enchantments.SOUL_SPEED).inLoottable(bastionRewards).rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.SWIFT_SNEAK).inLoottable("ancient_city", "ancient_city_ice_box").rarity(Rarity.EPIC),
+        new EnchantmentSettings(Enchantments.FROST_WALKER).inLoottable("chests/igloo_chest").dupedWithVanillaItem(Items.BLUE_ICE, 5),
+        new EnchantmentSettings(Enchantments.SOUL_SPEED).inLoottable(bastionRewards).rarity(Rarity.RARE).dupedWithVanillaItem(Items.GILDED_BLACKSTONE, 3),
+        new EnchantmentSettings(Enchantments.SWIFT_SNEAK).inLoottable("chests/ancient_city", "chests/ancient_city_ice_box").rarity(Rarity.EPIC).dupedWithVanillaItem(Items.SCULK_SENSOR),
 
         // bow enchantments
-        new EnchantmentSettings(Enchantments.INFINITY).inLoottable("jungle_temple").rarity(Rarity.UNCOMMON),
-        new EnchantmentSettings(Enchantments.POWER).inLoottable(bastionRewards).rarity(Rarity.RARE),
-        new EnchantmentSettings(Enchantments.PUNCH).inLoottable("jungle_temple").rarity(Rarity.UNCOMMON),
-        new EnchantmentSettings(Enchantments.FLAME).inLoottable("ruined_portal").rarity(Rarity.UNCOMMON),
+        new EnchantmentSettings(Enchantments.INFINITY).inLoottable("chests/jungle_temple").rarity(Rarity.UNCOMMON).dupedWithVanillaItem(Items.SENTRY_ARMOR_TRIM_SMITHING_TEMPLATE),
+        new EnchantmentSettings(Enchantments.POWER).inLoottable(bastionRewards).rarity(Rarity.RARE), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.PUNCH).inLoottable("chests/jungle_temple").rarity(Rarity.UNCOMMON), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.FLAME).inLoottable("chests/ruined_portal").rarity(Rarity.UNCOMMON).dupedWithVanillaItem(Items.MAGMA_CREAM),
 
         // fishing enchantments
-        new EnchantmentSettings(Enchantments.LUCK_OF_THE_SEA).inLoottable("buried_treasure"),
-        new EnchantmentSettings(Enchantments.LURE), // TODO: From fishing
+        new EnchantmentSettings(Enchantments.LUCK_OF_THE_SEA).inLoottable("chests/buried_treasure"), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.LURE), // TODO: From fishing // TODO: dupeability
 
         // crossbow enchantments
-        new EnchantmentSettings(Enchantments.MULTISHOT).inLoottable("pillager_outpost"),
-        new EnchantmentSettings(Enchantments.PIERCING).inLoottable("pillager_outpost"),
-        new EnchantmentSettings(Enchantments.QUICK_CHARGE).inLoottable("pillager_outpost"),
+        new EnchantmentSettings(Enchantments.MULTISHOT).inLoottable("chests/pillager_outpost"), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.PIERCING).inLoottable("chests/pillager_outpost"), // TODO: dupeability
+        new EnchantmentSettings(Enchantments.QUICK_CHARGE).inLoottable("chests/pillager_outpost").dupedWithVanillaItem(Items.FIREWORK_STAR),
 
         // curses
-        new EnchantmentSettings(Enchantments.VANISHING_CURSE).maxEnchTableLvl(1),
-        new EnchantmentSettings(Enchantments.BINDING_CURSE).maxEnchTableLvl(1),
-        new EnchantmentSettings(ModEnchantments.FRAGILITY_CURSE).maxEnchTableLvl(1)
+        new EnchantmentSettings(Enchantments.VANISHING_CURSE).maxEnchTableLvl(1).dupedWithVanillaItem(Items.CLOSED_EYEBLOSSOM),
+        new EnchantmentSettings(Enchantments.BINDING_CURSE).maxEnchTableLvl(1).dupedWithVanillaItem(Items.ALLIUM),
+        new EnchantmentSettings(ModEnchantments.FRAGILITY_CURSE).maxEnchTableLvl(1).dupedWithVanillaItem(Items.BLUE_ORCHID)
     );
 
-    public static final Map<Identifier, List<EnchantmentSettings>> byLoottable = buildByLoottable();
+    public static Map<Identifier, List<EnchantmentSettings>> byLoottable = buildByLoottable();
     private static Map<Identifier, List<EnchantmentSettings>> buildByLoottable()
     {
         Map<Identifier, List<EnchantmentSettings>> map = new HashMap<>();
@@ -118,5 +126,31 @@ public class EnchantmentSettingsManager
     public static boolean isAllowedFromEnchantingTable(Holder<Enchantment> enchantment)
     {
         return enchantingTableAllowed.stream().anyMatch(enchantment::is);
+    }
+
+    public static Optional<EnchantmentSettings> getSettingsForEnch(Holder<Enchantment> enchantment) {
+        return enchantmentSettings.stream()
+                .filter(settings -> enchantment.is(settings.enchantment))
+                .findFirst();
+    }
+
+    public static void validateLoottables(HolderLookup.Provider registries) {
+        List<String> invalidLoottables = new ArrayList<>();
+
+        for (EnchantmentSettings settings : enchantmentSettings) {
+            List<Identifier> invalidForEnchantment = settings.validateLoottables(registries);
+
+            if (!invalidForEnchantment.isEmpty()) {
+                invalidLoottables.add(settings.enchantment.identifier() + ": " + invalidForEnchantment);
+            }
+        }
+
+        byLoottable = buildByLoottable();
+
+        if (!invalidLoottables.isEmpty()) {
+            throw new IllegalStateException(
+                "Enchantment settings reference unknown loot table(s): " + invalidLoottables
+            );
+        }
     }
 }
--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/modifiers/LecternLibrarianTrades.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/modifiers/LecternLibrarianTrades.java
@@ -1,22 +1,31 @@
 package uk.co.httpsmmuminecraftsociety.mainmod.modifiers;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.core.Holder;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.component.DataComponentExactPredicate;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.npc.villager.Villager;
 import net.minecraft.world.entity.npc.villager.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.ItemEnchantments;
 import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.block.entity.LecternBlockEntity;
+import uk.co.httpsmmuminecraftsociety.mainmod.enchantment.vanilla.EnchantmentSettings;
+import uk.co.httpsmmuminecraftsociety.mainmod.enchantment.vanilla.EnchantmentSettingsManager;
 
+import java.util.List;
 import java.util.Optional;
 
 public final class LecternLibrarianTrades {
-    private static final int MAX_USES = 999999;
-
     private LecternLibrarianTrades() {
     }
 
@@ -29,7 +38,7 @@ public final class LecternLibrarianTrades {
         offers.removeIf(LecternLibrarianTrades::isEnchantedBookOffer);
 
         getJobSiteEnchantedBook(villager, level)
-                .map(LecternLibrarianTrades::createCopyOffer)
+                .flatMap(book -> createCopyOffer(book, level))
                 .ifPresent(offer -> offers.add(0, offer));
     }
 
@@ -55,15 +64,100 @@ public final class LecternLibrarianTrades {
         return Optional.of(book.copyWithCount(1));
     }
 
-    private static MerchantOffer createCopyOffer(ItemStack book) {
-        return new MerchantOffer(
-                new ItemCost(Items.BOOK),
-                Optional.of(new ItemCost(Items.EMERALD_BLOCK)),
-                book,
+    private static Optional<MerchantOffer> createCopyOffer(ItemStack sourceBook, ServerLevel level) {
+        ItemEnchantments enchantments = sourceBook.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY);
+        if (enchantments.isEmpty()) {
+            return Optional.empty();
+        }
+
+        TradeCosts costs = calculateCosts(enchantments);
+        if (costs == null) {
+            return Optional.empty();
+        }
+
+        ItemStack result = sourceBook.copyWithCount(1);
+        if (enchantments.size() > 1)
+            addRandomCurse(result, level);
+
+        return Optional.of(new MerchantOffer(
+                currencyCost(costs.emeraldCost()),
+                Optional.of(itemCost(costs.dupeItem())),
+                result,
                 0,
-                MAX_USES,
+                999999,
                 0,
                 0.0F
+        ));
+    }
+
+    private static TradeCosts calculateCosts(ItemEnchantments enchantments) {
+        int emeraldCost = 0;
+        ItemStack dupeItem = ItemStack.EMPTY;
+
+        for (Object2IntMap.Entry<Holder<Enchantment>> entry : enchantments.entrySet()) {
+            Holder<Enchantment> enchantment = entry.getKey();
+            int level = entry.getIntValue();
+
+            Optional<EnchantmentSettings> settings = EnchantmentSettingsManager.getSettingsForEnch(enchantment);
+            if (settings.isEmpty() || !settings.get().hasDupeItem()) {
+                return null;
+            }
+
+            emeraldCost += enchantment.value().getAnvilCost() * level;
+            ItemStack ingredient = settings.get().createDupeItemStack(level);
+
+            if (dupeItem.isEmpty()) {
+                dupeItem = ingredient;
+            } else if (ItemStack.isSameItemSameComponents(dupeItem, ingredient)) {
+                dupeItem.grow(ingredient.getCount());
+            }
+        }
+
+        if (enchantments.size() > 1) {
+            emeraldCost *= 2;
+        }
+
+        return new TradeCosts(Math.max(1, emeraldCost), dupeItem);
+    }
+
+    private static ItemCost currencyCost(int emeraldCost) {
+        if (emeraldCost <= 64) {
+            return new ItemCost(Items.EMERALD, emeraldCost);
+        }
+
+        return new ItemCost(Items.EMERALD_BLOCK, Math.max(1, Math.ceilDiv(emeraldCost, 9)));
+    }
+
+    private static ItemCost itemCost(ItemStack stack) {
+        if (stack.getComponentsPatch().isEmpty()) {
+            return new ItemCost(stack.getItem(), stack.getCount());
+        }
+
+        return new ItemCost(
+                stack.typeHolder(),
+                stack.getCount(),
+                DataComponentExactPredicate.allOf(stack.getComponents()),
+                stack
         );
     }
+
+    private static void addRandomCurse(ItemStack book, ServerLevel level) {
+        ItemEnchantments enchantments = book.getOrDefault(DataComponents.STORED_ENCHANTMENTS, ItemEnchantments.EMPTY);
+        ItemEnchantments.Mutable mutable = new ItemEnchantments.Mutable(enchantments);
+        List<Holder.Reference<Enchantment>> curses = EnchantmentSettingsManager.curses.stream()
+                .map(curse -> level.registryAccess()
+                        .lookupOrThrow(Registries.ENCHANTMENT)
+                        .getOrThrow(curse))
+                .toList();
+
+        if (curses.isEmpty()) {
+            return;
+        }
+
+        RandomSource random = level.getRandom();
+        mutable.set(curses.get(random.nextInt(curses.size())), 1);
+        book.set(DataComponents.STORED_ENCHANTMENTS, mutable.toImmutable());
+    }
+
+    private record TradeCosts(int emeraldCost, ItemStack dupeItem) {}
 }

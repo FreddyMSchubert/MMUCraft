dcc2e7e808760dc293e9e5ccd937053a0c4058d9
FreddyMSchubert <github@frederickschubert.de>
2026-08-14 12:47:35 +0200

    charms drop instead of being destroyed when an armor piece breaks, closes #98

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/mixin/RefreshCharmOnArmorEquip.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/mixin/RefreshCharmOnArmorEquip.java
@@ -1,13 +1,21 @@
 package uk.co.httpsmmuminecraftsociety.mainmod.mixin;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.FakeItems;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.CharmorManager;
 import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.CharmsManager;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.StoredCharmData;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.fakeItemDefs.FakeItem;
+
+import java.util.List;
 
 @Mixin(LivingEntity.class)
 public class RefreshCharmOnArmorEquip
@@ -15,5 +23,25 @@ public class RefreshCharmOnArmorEquip
     @Inject(method = "onEquipItem", at = @At("HEAD"))
     private void refreshCharm(EquipmentSlot slot, ItemStack oldStack, ItemStack newStack, CallbackInfo info) {
         if (slot.isArmor()) CharmsManager.refresh(newStack);
+    }
+
+    @Inject(method = "onEquippedItemBroken", at = @At("RETURN"))
+    private void recoverArmorCharms(Item item, EquipmentSlot slot, CallbackInfo info) {
+        if (!slot.isArmor() || !((Object) this instanceof ServerPlayer player)) return;
+
+        List<StoredCharmData> storedCharms = CharmorManager.getStoredArmorCharms(player.getItemBySlot(slot));
+        boolean slotIsEmpty = true;
+        for (StoredCharmData storedCharm : storedCharms) {
+            FakeItem fakeItem = FakeItems.CHARM_ID_MAP.get(storedCharm.charmId());
+            if (fakeItem == null) continue;
+
+            ItemStack charm = fakeItem.createItemStackAtLevel(storedCharm.level());
+            if (slotIsEmpty) {
+                player.setItemSlot(slot, charm);
+                slotIsEmpty = false;
+            } else if (!player.getInventory().add(charm)) {
+                player.drop(charm, false);
+            }
+        }
     }
 }

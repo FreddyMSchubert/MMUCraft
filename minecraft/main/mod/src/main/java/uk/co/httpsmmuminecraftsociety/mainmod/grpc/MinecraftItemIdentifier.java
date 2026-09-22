07e1e9fe21d5d20175b4d4a89c7ce7ca281c12da
FreddyMSchubert <github@frederickschubert.de>
2026-09-22 13:46:26 +0100

    fix enderite not rendering on the website stuff

--- minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/grpc/MinecraftItemIdentifier.java
+++ minecraft/main/mod/src/main/java/uk/co/httpsmmuminecraftsociety/mainmod/grpc/MinecraftItemIdentifier.java
@@ -3,6 +3,7 @@ package uk.co.httpsmmuminecraftsociety.mainmod.grpc;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ItemStack;
 import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.FakeItems;
+import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.charms.CharmorManager;
 import uk.co.httpsmmuminecraftsociety.mainmod.fakeItems.fakeItemDefs.FakeItem;
 
 final class MinecraftItemIdentifier {
@@ -10,8 +11,14 @@ final class MinecraftItemIdentifier {
 
     static String forStack(ItemStack stack) {
         FakeItem fakeItem = FakeItems.getFakeItemFromStack(stack);
-        return fakeItem == null
-                ? BuiltInRegistries.ITEM.getKey(stack.getItem()).toString()
-                : "mainmod:" + fakeItem.id();
+        if (fakeItem != null) return "mainmod:" + fakeItem.id();
+        String itemId = BuiltInRegistries.ITEM.getKey(stack.getItem()).toString();
+        if (itemId.equals("minecraft:test_block")) return "mainmod:alien-debris";
+        if (itemId.equals("minecraft:test_instance_block")) return "mainmod:enderite-block";
+        // Enderite gear shares Netherite item IDs; preserve its marker for web previews.
+        if (CharmorManager.isEnderite(stack) && itemId.startsWith("minecraft:netherite_")) {
+            return "mainmod:enderite-" + itemId.substring("minecraft:netherite_".length()).replace('_', '-');
+        }
+        return itemId;
     }
 }
